c9546f8d53edf34e00be54300c8667dc20e8ac25
Phuc18CPP01 <ip6sphucnguyen1999@gmail.com>
2018-12-07 19:06:30 +0700

    BTVN

---
+++ Bài tập Tổng.Tich or Tích.tổng/Tong(1+(1+2)..(1+n)).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+int main()
+{	int n=0,s=0,p=1;
+cout<< "nhap tu  ban phim n ";
+cin>>n;
+if (n<1) { cout<<"vui long nhap lon hon  or bang 1:";}
+else
+for (int i=1;i<=n;i++) { s+=i;
+p*=s;}
+cout<<"Tong la "<<p;}
+
+
